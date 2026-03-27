068ddca5ec86be22150540cf27e8c57e24dd623d
Elidiane <121459020+elidianesampaiotrabuco@users.noreply.github.com>
2026-03-27 12:15:03 -0300

    added main menu patch (sorry thumbly)
    this patch changes the copyright and version texts to the one related to the mod
    
    @Denyscrasav4ik read the comment on line 6

---
+++ ModPatches/MenuPatch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace BBTimes.ModPatches // sorry if i stole code from bbpoop. denys, give credit to thumbly for the main menu patch
+{
+    [HarmonyPatch(typeof(MainMenu))]
+    [HarmonyPatch("Start")]
+    internal class MenuPatch
+    {
+        static void Postfix(MainMenu __instance)
+        {
+            __instance.gameObject.transform.Find("Copyright").GetComponent<TextMeshProUGUI>().text = "2023-2026 EkremTheStickman\nDeveloped by DenysCrasav4ik";
+            __instance.gameObject.transform.Find("Version").GetComponent<TextMeshProUGUI>().text = "V1.4";
+
+        }
+    }
+}
